32855717f9c5fb70998d1dbfaa09df93c5551a79
G-Kafal <grivenkafalinji51@gmail.com>
2025-05-19 15:37:12 +0200

    Initial commit

---
+++ src/main/java/pl/edu/vistula/first_project_java_spring/ClassProjectApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.vistula.first_project_java_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassProjectApplication.class, args);
+	}
+
+}
---
+++ src/main/java/pl/edu/vistula/first_project_java_spring/HelloController.java
@@ -0,0 +1,4 @@
+package pl.edu.vistula.first_project_java_spring;
+
+public class HelloController {
+}
